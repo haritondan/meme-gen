e9819251968737093116d55fa0365ca45a482e40
DanHariton <danhariton22@gmail.com>
2024-04-27 13:10:15 +0300

    fix:add download option

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import MemeGenerator from './Components/meme-generator/meme-generator';
--- src/Components/meme-display/meme-display.tsx
+++ src/Components/meme-display/meme-display.tsx
@@ -7,11 +7,47 @@ interface MemeDisplayProps {
 }
 
 const MemeDisplay: React.FC<MemeDisplayProps> = ({ topCaption, bottomCaption, imageUrl }) => {
+  const handleDownload = () => {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+
+    if (!ctx) {
+      console.error('Canvas context is null or undefined.');
+      return;
+    }
+
+    const img = new Image();
+    img.crossOrigin = 'anonymous';
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+
+      ctx.drawImage(img, 0, 0);
+      ctx.fillStyle = 'white';
+      ctx.font = 'bold 36px Arial';
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'top';
+      ctx.fillText(topCaption, canvas.width / 2, 10);
+      ctx.fillText(bottomCaption, canvas.width / 2, canvas.height - 50);
+
+      const dataUrl = canvas.toDataURL('image/png');
+      const link = document.createElement('a');
+      link.download = 'meme.png';
+      link.href = dataUrl;
+      link.click();
+    };
+
+    img.src = imageUrl;
+  };
+
   return (
-    <div className="meme-container">
-      <img src={imageUrl} alt="Meme" />
-      <div className="top-caption">{topCaption}</div>
-      <div className="bottom-caption">{bottomCaption}</div>
+    <div className="meme-display-container">
+      <div className="meme-container">
+        <img src={imageUrl} alt="Meme" />
+        <div className="top-caption">{topCaption}</div>
+        <div className="bottom-caption">{bottomCaption}</div>
+      </div>
+      <button onClick={handleDownload} className='downloadButton'>Download</button>
     </div>
   );
 };
